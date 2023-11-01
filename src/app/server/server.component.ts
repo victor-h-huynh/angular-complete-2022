233cdf37ff7c97263d42996dd90e1587dec47db3
victor-h-huynh <victor.h.huynh@gmail.com>
2023-11-01 11:54:01 -0400

    string-interpolation

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -6,10 +6,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./server.component.css']
 })
 export class ServerComponent implements OnInit {
+  serverId: number = 10;
+  serverStatus: string = 'offline'
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  getServerStatus() {
+    return this.serverStatus
   }
 
 }
