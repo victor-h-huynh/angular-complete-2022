bf102a6661c30797a4b807d6ae9c77d37b0b7b0a
victor-h-huynh <victor.h.huynh@gmail.com>
2023-11-14 13:44:00 -0500

    host-binding

---
+++ SECTIONS/directives-start/src/app/better-highlight/better-highlight.directive.ts
@@ -0,0 +1,22 @@
+import { Directive, Renderer2, OnInit, ElementRef, HostListener } from '@angular/core';
+
+@Directive({
+  selector: '[appBetterHighlight]'
+})
+export class BetterHighlightDirective implements OnInit {
+
+
+  constructor(private elRef: ElementRef, private renderer: Renderer2) { }
+
+  ngOnInit(): void {
+    // this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'blue');
+  }
+
+  @HostListener('mouseenter') mouseover(eventData: Event) {
+    this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'blue');
+  }
+
+  @HostListener('mouseleave') mouseleave(eventData: Event) {
+    this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'transparent');
+  }
+}
